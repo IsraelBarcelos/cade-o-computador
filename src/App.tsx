a672c0c0d83110f872ed3c1963498cce26a839b9
Israel da Rosa Barcelos <israelbarcelos7@gmail.com>
2021-06-30 22:29:04 -0300

    Pages basic structure added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { BrowserRouter, Route } from 'react-router-dom'; 
+import { Login } from './pages/Login'
+import { Andares } from './pages/Andares' 
+import { Andar } from './pages/Andar'
 function App() {
   return (
-    <div>
-    </div>
+    <BrowserRouter>
+      <Route path="/" exact component={Login}></Route>
+      <Route path="/andares" exact component={Andares}></Route>
+      <Route path="/andar" exact component={Andar}></Route>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/MonitorENobreak.tsx
@@ -0,0 +1,6 @@
+export function MonitorENobreak() {
+  return(
+    <>
+    </>
+  )
+}
